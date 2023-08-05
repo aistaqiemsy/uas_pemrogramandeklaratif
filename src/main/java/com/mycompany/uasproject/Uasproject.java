fdb9603189344cd382b52fa72f7ce14857ae3ce8
aistaqiemsy <a.istakim@gmail.com>
2023-08-05 10:34:39 +0700

    UAS Pemrograman Deklaratif

---
+++ src/main/java/com/mycompany/uasproject/Uasproject.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uasproject;
+
+/**
+ *
+ * @author maulh
+ */
+public class Uasproject {
+
+    public static void main(String[] args) {
+    //        System.out.println("Hello World!");
+    mainMenu menuUtama = new mainMenu();
+    menuUtama.setVisible(true);
+    }
+}
